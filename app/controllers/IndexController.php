74293aad4cf93432fb46cd25a58b4bbfb6d76ce7
Munoz Daniel <munozda87@hotmail.com>
2015-09-27 10:53:31 -0300

    IndexController: Eliminado la insercion en el footer del miniSlider.js

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -7,18 +7,7 @@ class IndexController extends ControllerBase
         $this->tag->setTitle('El Placer de vivir');
         parent::initialize();
         //cargar los js para la vista de esta función
-        $this->assets
-            ->collection('footer')
-            ->addJs('js/mini/miniSlider.js');
-        $this->assets
-            ->collection('footerInline')
-            ->addInlineJs('$(function() {
-            $(".slider").miniSlider({
-             pauseOnHover: true,
-             showNavigation: false, previousBtnClass: "prev",nextBtnContent: "next",
-              showPagination: false
-             });
-            });');
+
     }
 
     public function indexAction()
