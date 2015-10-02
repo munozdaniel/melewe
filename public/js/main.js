5325c4ed7ec9740a6d5854c4cb248832f7c95a30
Munoz Daniel <munozda87@hotmail.com>
2015-10-01 22:41:14 -0300

    Slider: Agregado las imagenes

--- public/js/main.js
+++ public/js/main.js
@@ -77,12 +77,9 @@ BRUSHED.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{image : 'img/slider-images/image01.jpg', title : '<div class="slide-content font-melewe">melewe</div>', thumb : '', url : ''},
-											{image : 'img/slider-images/image02.jpg', title : '<div class="slide-content font-melewe">melewe</div>', thumb : '', url : ''},
-											{image : 'img/slider-images/image02.jpg', title : '<div class="slide-content font-melewe">melewe</div>', thumb : '', url : ''},
-											{image : 'img/slider-images/image02.jpg', title : '<div class="slide-content font-melewe">melewe</div>', thumb : '', url : ''},
-											{image : 'img/slider-images/image03.jpg', title : '<div class="slide-content font-melewe">melewe</div>', thumb : '', url : ''},
-											{image : 'img/slider-images/image04.jpg', title : '<div class="slide-content font-melewe">melewe</div>', thumb : '', url : ''}
+											{image : 'img/slider-images/image01.jpg', title : '<div class="slide-content font-melewe" style="font-size: 115px">melewe</div>', thumb : '', url : ''},
+											{image : 'img/slider-images/image02.jpg', title : '<div class="slide-content font-melewe" style="font-size: 115px">melewe</div>', thumb : '', url : ''},
+											{image : 'img/slider-images/image04.jpg', title : '<div class="slide-content font-melewe" style="font-size: 115px">melewe</div>', thumb : '', url : ''}
 									],
 									
 		// Theme Options			   
@@ -124,7 +121,7 @@ BRUSHED.filter = function (){
 		// filter items when filter link is clicked
 		var $optionSets = $('#options .option-set'),
 			$optionLinks = $optionSets.find('a');
-	
+
 		  $optionLinks.click(function(){
 			var $this = $(this);
 			// don't proceed if already selected
@@ -134,7 +131,7 @@ BRUSHED.filter = function (){
 			var $optionSet = $this.parents('.option-set');
 			$optionSet.find('.selected').removeClass('selected');
 			$this.addClass('selected');
-	  
+
 			// make option object dynamically, i.e. { filter: '.my-filter-class' }
 			var options = {},
 				key = $optionSet.attr('data-option-key'),
@@ -149,7 +146,7 @@ BRUSHED.filter = function (){
 			  // otherwise, apply new options
 			  $container.isotope( options );
 			}
-			
+
 			return false;
 		});
 	}
@@ -170,7 +167,7 @@ BRUSHED.fancyBox = function(){
 					this.title = '<h4>' + this.title + '</h4>' + '<p>' + $(this.element).parent().find('img').attr('alt') + '</p>';
 				},
 				helpers : {
-					title : { type: 'inside' },
+					title : { type: 'inside' }
 				}
 			});
 			
